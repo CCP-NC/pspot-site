f0f01da43a11d2b673f9e6bba1a0b8cee4333012
ssturniolo <4365814+stur86@users.noreply.github.com>
2016-01-12 18:39:43 +0000

    Completed preprocessing script

--- python_scripts/pspot_process.py
+++ python_scripts/pspot_process.py
@@ -37,8 +37,78 @@ def run_castep_calc(ppot):
     open(cell_fname, 'w').write(cell_file)
 
     # Now run a CASTEP dryrun
-    castep_run = subprocess.Popen([config['castep_command'], '-dryrun', ppot.name], cwd=dirname)
-    castep_run.wait()
+    try:
+        castep_run = subprocess.Popen([config['castep_command'], '-dryrun', ppot.name], cwd=dirname)
+        castep_run.wait()
+    except OSError:
+        sys.stderr.write("CASTEP run failed, skipping\n")
+        return
+
+def parse_agrfile(fname):
+
+    # Parse a Grace file returning graphs, datasets for each of them, and additional info
+    agrf = open(fname).readlines()
+    agrobj = {}
+
+    graphline_re = re.compile('@[gG]([0-9]+)[\s]+on')
+    withline_re = re.compile('@with[\s]+[gG]([0-9]+)')
+    propline_re = re.compile('@[\s]+([\w\s]+)[\s]+([\+\-0-9\.]+|"[^"]*"|off|on)')
+    targline_re = re.compile('@target[\s]+[gG]([0-9]+)\.[sS]([0-9]+)')
+    typeline_re = re.compile('@type[\s]+xy')
+    dataline_re = re.compile('[\s]*([0-9eE\-\.]+)[\s]+([0-9eE\-\.]+)')
+
+    current_with = None
+    current_target = None
+
+    for l in agrf:
+
+        if current_with is not None:
+            pm = propline_re.findall(l)
+            if len(pm) != 1:
+                current_with = None
+                continue
+            # Else, split the line into a series of properties in tree fashion
+            prop_tree = ["with"] + pm[0][0].strip().split()
+            prop_val = pm[0][1]
+            def seq_tree_set(obj, tree, val):
+                if len(tree) == 1:
+                    obj[tree[0]] = val
+                else:
+                    if tree[0] not in obj:
+                        obj[tree[0]] = {}
+                    seq_tree_set(obj[tree[0]], tree[1:], val)
+            seq_tree_set(agrobj[current_with], prop_tree, prop_val)
+        elif current_target is not None:
+            if typeline_re.match(l) is not None:
+                continue
+            dm = dataline_re.findall(l)
+            if len(dm) != 1:
+                current_target = None
+                continue
+            data = [float(d) for d in dm[0]]
+            agrobj[current_target[0]]['sets'][current_target[1]].append(data)
+        else:
+            # Check if we have a graph, a with, or a target
+            gm = graphline_re.findall(l)
+            if len(gm) > 0:
+                # Graph! Add it to the obj
+                agrobj[int(gm[0])] = {'sets': {}, 'with': {}}
+                continue
+            wm = withline_re.findall(l)
+            if len(wm) > 0:
+                # With! Set the current_with, only if present
+                if int(wm[0]) in agrobj:
+                    current_with = int(wm[0])
+                continue
+            tm = targline_re.findall(l)
+            if len(tm) > 0:
+                # Target! Set the current_target, only if graph is present
+                if int(tm[0][0]) in agrobj:
+                    current_target = (int(tm[0][0]), int(tm[0][1]))
+                    agrobj[current_target[0]]['sets'][current_target[1]] = []
+                continue
+
+    return agrobj
 
 def parse_betaproj(ppot):
 
@@ -50,21 +120,12 @@ def parse_betaproj(ppot):
     beta_fname = os.path.join(dirname, ppot.name+'.beta')
 
     try:
-        beta_lines = open(beta_fname).readlines()
+        beta_obj = parse_agrfile(beta_fname)
     except IOError:
         sys.stderr.write("Could not parse beta projectors for pseudopotential {0}\n".format(ppot.name))
         return None
 
-    beta_proj = {}
-
-    # First identify how many projectors are defined and where
-    on_re = re.compile("@g([0-9]+)[\s]+on")
-
-    for l in beta_lines:
-        on_match = on_re.findall(l)
-        if len(on_match) > 0:
-            print on_match
-
+    return beta_obj
 
 # Parsing command line arguments
 
@@ -82,7 +143,7 @@ config = {
     "pspot_default_termination": "OTF",
     "graph_path": "graphs",
     "castep_command": "castep.serial",
-    "cell_template": "template.cell"
+    "cell_template": "template.cell",
 } # Default settings
 
 # The directory of the project, calculated relative to the physical location of the script
@@ -149,6 +210,8 @@ if args.plot and cell_template is not None:
     # Clear the folder
     clear_folder(os.path.join(main_abspath, config['graph_path']))
 
+    agr_extensions = ['beta', 'econv', 'pwave']
+
     for p_i, ppot in enumerate(pspot_list):
 
         sys.stdout.write("Processing pseudopotential file {0} of {1}\n".format(p_i+1, len(pspot_list)))
@@ -158,4 +221,19 @@ if args.plot and cell_template is not None:
         sys.stdout.write("Running CASTEP calculation\n")
         run_castep_calc(ppot)
         sys.stdout.write("Plotting graphs\n")
-        parse_betaproj(ppot)
+        dirname = os.path.join(main_abspath, config['graph_path'], ppot.name)        
+        for ext in agr_extensions:
+            fname = os.path.join(dirname, ppot.el+'_OTF.'+ext)
+            try:
+                agr_obj = parse_agrfile(fname)
+            except IOError:
+                sys.stderr.write("Could not parse {0} file for pseudopotential {1}\n".format(ext, ppot.name))
+
+            # Now dump it as json file
+            json.dump(agr_obj, open(os.path.join(dirname, ext + '.json'), 'w'))
+            # Also create an SVG with Grace for good measure
+            try:
+                xmgrace_run = subprocess.Popen(['xmgrace', fname, '-hdevice', 'SVG', '-hardcopy', '-printfile', os.path.join(dirname, ext+'.svg')])
+                xmgrace_run.wait()
+            except OSError:
+                sys.stderr.write("Grace not installed, SVG plots can not be generated\n")
