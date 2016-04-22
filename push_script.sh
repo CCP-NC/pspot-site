6b71d2738b9fb53b2f1fd89eae3d3c1dd41b06ed
ssturniolo <simone.sturniolo@gmail.com>
2016-04-22 15:27:22 +0100

    Fixed a small problem with the help button in pspot.html, improved the push_script

--- push_script.sh
+++ push_script.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 # A script to push the website and its content up to a given URL
-FILE_LIST="graphs data pspot_data.json"
+DATA_LIST="graphs data pspot_data.json"
+SITE_LIST="index.html pspot.html help stylesheet.css favicon.ico js lib"
 HELP_LAYOUT="github"
+DEFAULT_URL="ccpnc@webs.materials.ox.ac.uk:public_html/pspot_site"
 
 echo "What to push?"
-select pmode in "All" "Just library data"; do
+select pmode in "All" "Just library data" "Just website"; do
 	case $pmode in
 		"All" ) # Add all the other files
 			# First, compile help
 			generate-md --layout $HELP_LAYOUT --input help.md --output help
-			FILE_LIST=$FILE_LIST" index.html pspot.html help stylesheet.css favicon.ico js lib"
+			FILE_LIST=$DATA_LIST" "$SITE_LIST
 			break;;	
-		"Just library data" ) break;;
+		"Just library data" ) 
+			FILE_LIST=$DATA_LIST
+			break;;
+		"Just website" )
+			generate-md --layout $HELP_LAYOUT --input help.md --output help
+			FILE_LIST=$SITE_LIST
+			break;;			
 	esac
 done
 
@@ -22,7 +30,17 @@ CONFIRM="n"
 while 
 	[ $CONFIRM != "y" ]
 do
-	read -p "Enter the destination URL: " URL
+	echo "Push to default URL "$DEFAULT_URL" ?"
+	select umode in "Yes" "No"; do
+		case $umode in
+			"Yes" )
+				URL=$DEFAULT_URL
+				break;;
+			"No" )
+				read -p "Enter the destination URL: " URL
+				break;;
+		esac
+	done
 	echo "You are about to copy the following files: "
 	echo $FILE_LIST
 	echo "to the following URL: "
