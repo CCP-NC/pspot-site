1c2dac7a333e470701c672f747afe00f0f58c782
Simone Sturniolo <simonesturniolo@gmail.com>
2019-10-18 16:54:06 +0100

    Added description parsing to f90data2yml script

--- python_scripts/f90data2yml.py
+++ python_scripts/f90data2yml.py
@@ -9,20 +9,26 @@ import yaml
 infile = open(sys.argv[1]).readlines()
 
 lib = {}
+libdesc_re = re.compile("ps_set = ([0-9]+)\s+->(.+)")
 libhead_re = re.compile("ps_lib[\(\),0-9\s]+/\"([a-zA-Z0-9]+)\"")
 librow_re = re.compile("ps_def[\(\),0-9\s]+/\"([^\"]*)\"/ !\*[\s]+([A-Za-z]+)")
 
 header = None
+desc = ''
 
 for l in infile:
+	match_desc = libdesc_re.findall(l)
 	match_head = libhead_re.findall(l)
 	match_row = librow_re.findall(l)
 
+	if len(match_desc) > 0:
+		desc = match_desc[0][1].strip()
+
 	if len(match_head) > 0:
 		header = match_head[0]
-		lib[header] = {}
+		lib[header] = {'description': desc, 'elements': {}}
 
 	if header is not None and len(match_row) > 0:
-		lib[header][match_row[0][1]] = match_row[0][0]
+		lib[header]['elements'][match_row[0][1]] = match_row[0][0]
 
 print(yaml.safe_dump(lib, default_flow_style=False))
